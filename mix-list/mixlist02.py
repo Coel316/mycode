5be2e35edeac35f583886e41992d22c5656cb62e
Coel316 <coel316@yahoo.com>
2022-11-08 23:39:08 +0000

    manipulating lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Alta3 Research | TPatrick
+    Lists Challenge """# import random module
+import random
+
+def main():
+
+    # enter variable data
+    wordbank = ["indentation", "spaces"]
+    tlgstudents= ["Aaron", "Andy", "Asif",
+                  "Brent", "Cedric", "Chris",
+                  "Cory", "Ebrima", "Fran"]
+
+# print the list as shown above. This helps to compare differences later implemented.
+    print(tlgstudents)
+
+# append the integer 4 to the wordbank list and then print the new list
+    wordbank.append(4)
+    print(wordbank)
+
+
+main()
