d03f32e884393d64233d91b53598f31b1ffe95b9
Coel316 <coel316@yahoo.com>
2022-11-10 21:21:26 +0000

    corrected if statements, updated comment for computer input

--- projects/firstproject.py
+++ projects/firstproject.py
@@ -9,7 +9,7 @@ currentdate = datetime.now()
 print("")
 print('The current date is:', currentdate,".\n")
 
-# acquire today's date and convert to a number for day of week
+# receive today's date from computer and convert to a number for day of week
 currentdayofweek = currentdate.isoweekday()
 print('There are 7 days in a week.  Today is #', currentdayofweek)
 print("")
@@ -19,11 +19,11 @@ while(currentdayofweek < 7):
 
 # date / time library starts with 0 being Sunday.
     if (currentdayofweek == 0):
-        print("The day of the week is Sunday")
+        print("The day of the week is Sunday\n")
         break # break(s) are in b/c it kept looping
 
     elif(currentdayofweek == 1):
-        print("The day of the week is Monday")
+        print("The day of the week is Monday\n")
         break
 
     elif(currentdayofweek == 2):
